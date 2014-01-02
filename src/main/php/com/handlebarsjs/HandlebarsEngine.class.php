30ef8a9d8b7e69ea1c6507218a9fb89292fd3c67
Timm Friebe <thekid@thekid.de>
2014-01-02 12:32:13 +0100

    Add "each" builtin helper

--- src/main/php/com/handlebarsjs/HandlebarsEngine.class.php
+++ src/main/php/com/handlebarsjs/HandlebarsEngine.class.php
@@ -3,5 +3,34 @@
 use com\github\mustache\MustacheEngine;
 
 class HandlebarsEngine extends MustacheEngine {
-  
+  protected $builtin= array();
+
+  /**
+   * Constructor. Initializes builtin helpers.
+   */
+  public function __construct() {
+    parent::__construct();
+    $this->builtin['each']= function($items, $context, $options) {
+      $list= $context->lookup($options[0]);
+      if ($context->isList($list)) {
+        $out= '';
+        foreach ($list as $element) {
+          $out.= $items->evaluate($context->asContext($element));
+        }
+        return $out;
+      } else {
+        return '';
+      }
+    };
+  }
+
+  /**
+   * Sets helpers
+   *
+   * @param  [:var] $helpers
+   * @return self this
+   */
+  public function withHelpers(array $helpers) {
+    return parent::withHelpers(array_merge($this->builtin, $helpers));
+  }
 }
