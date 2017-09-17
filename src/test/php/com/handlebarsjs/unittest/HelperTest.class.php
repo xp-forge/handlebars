413ca526e8b0a21c0fd26e9d2b1c733df5b6af54
Timm Friebe <thekid@thekid.de>
2017-09-17 11:41:14 +0200

    Add possibility to test with templates

--- src/test/php/com/handlebarsjs/unittest/HelperTest.class.php
+++ src/test/php/com/handlebarsjs/unittest/HelperTest.class.php
@@ -1,8 +1,15 @@
 <?php namespace com\handlebarsjs\unittest;
 
 use com\handlebarsjs\HandlebarsEngine;
+use com\github\mustache\InMemory;
 
 abstract class HelperTest extends \unittest\TestCase {
+  protected $templates;
+
+  /** @return void */
+  public function setUp() {
+    $this->templates= new InMemory();
+  }
 
   /**
    * Evaluate a string template against given variables and return the output.
@@ -12,6 +19,6 @@ abstract class HelperTest extends \unittest\TestCase {
    * @return string
    */
   protected function evaluate($template, $variables) {
-    return (new HandlebarsEngine())->render($template, $variables);
+    return (new HandlebarsEngine())->withTemplates($this->templates)->render($template, $variables);
   }
 }
