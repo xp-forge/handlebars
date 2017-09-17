7e7f438ec632a01dd5e02561d6b31f3603d31820
Timm Friebe <thekid@thekid.de>
2017-09-17 20:28:33 +0200

    Extend com.handlebarsjs.unittest.PartialBlockHelperTest::layout() test

--- src/test/php/com/handlebarsjs/unittest/PartialBlockHelperTest.class.php
+++ src/test/php/com/handlebarsjs/unittest/PartialBlockHelperTest.class.php
@@ -21,12 +21,30 @@ class PartialBlockHelperTest extends HelperTest {
 
   #[@test]
   public function layout() {
-    $this->templates->add('layout', '<head><title>{{title}}</title>{{#> head}}{{/head}}</head>');
-    $this->assertEquals('<head><title>Home</title><link rel="stylesheet" href="style.css"></head>', $this->evaluate(
-      '{{#> layout title="Home"}}'.
-        '{{#*inline "head"}}<link rel="stylesheet" href="style.css">{{/inline}}'.
-      '{{/layout}}',
-      []
-    ));
+    $this->templates->add('includes/hero', '<div class="hero"><img src="{{hero-src}}" alt="{{hero-alt}}"/></div>');
+    $this->templates->add('layout', trim('
+      <html>
+        <head><title>{{title}}</title>{{#> head}}{{/head}}</head>
+        <body>
+          {{#> hero}}(No hero){{/hero}}
+        </body>
+      </html>
+    '));
+
+    $this->assertEquals(trim('
+      <html>
+        <head><title>Home</title><link rel="stylesheet" href="style.css"></head>
+        <body>
+          <div class="hero"><img src="hero.jpg" alt="Hero 1 alt title"/></div>
+        </body>
+      </html>'),
+      $this->evaluate(trim('
+        {{#> layout title="Home"}}
+          {{#*inline "head"}}<link rel="stylesheet" href="style.css">{{/inline}}
+          {{#*inline "hero"}}{{> includes/hero hero-src="hero.jpg" hero-alt="Hero 1 alt title"}}{{/inline}}
+        {{/layout}}'),
+        []
+      )
+    );
   }
 }
