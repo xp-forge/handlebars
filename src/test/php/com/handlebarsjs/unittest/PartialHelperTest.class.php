764bd2f71b76b7d419cdfce77a8c42757eee2b6f
Timm Friebe <thekid@thekid.de>
2019-05-14 13:17:42 +0200

    Verify dynamic partials work as advertised in HandleBars docs
    See https://handlebarsjs.com/partials.html

--- src/test/php/com/handlebarsjs/unittest/PartialHelperTest.class.php
+++ src/test/php/com/handlebarsjs/unittest/PartialHelperTest.class.php
@@ -9,6 +9,18 @@ class PartialHelperTest extends HelperTest {
   }
 
   #[@test]
+  public function dynamic_partial() {
+    $this->templates->add('layout', 'My layout');
+    $this->assertEquals('My layout', $this->evaluate('{{> (whichPartial)}}', ['whichPartial' => 'layout']));
+  }
+
+  #[@test]
+  public function dynamic_partial_via_lookup() {
+    $this->templates->add('layout', 'My layout');
+    $this->assertEquals('My layout', $this->evaluate('{{> (lookup . "whichPartial")}}', ['whichPartial' => 'layout']));
+  }
+
+  #[@test]
   public function partial_contexts() {
     $this->templates->add('layout', 'My layout for {{name.en}}');
     $this->assertEquals('My layout for Tool', $this->evaluate('{{> layout theme}}', [
