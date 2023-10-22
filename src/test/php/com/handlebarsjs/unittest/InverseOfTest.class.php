26a3d6d580d43e5268b6efcbe352dbd7aa72b80b
Timm Friebe <thekid@thekid.de>
2023-10-22 13:22:04 +0200

    Add tests for inverse sections
    See https://github.com/handlebars-lang/handlebars.js/blob/master/spec/blocks.js#L144

--- src/test/php/com/handlebarsjs/unittest/InverseOfTest.class.php
+++ src/test/php/com/handlebarsjs/unittest/InverseOfTest.class.php
@@ -4,10 +4,42 @@ use test\{Assert, Test, Values};
 
 class InverseOfTest extends HelperTest {
 
+  #[Test, Values([[[], 'no people'], [['A', 'B'], 'AB']])]
+  public function inverse_section($input, $outcome) {
+    Assert::equals($outcome, $this->evaluate(
+      '{{#people}}{{this}}{{/people}}{{^people}}no people{{/people}}',
+      ['people' => $input]
+    ));
+  }
+
+  #[Test, Values([[[], 'no people'], [['A', 'B'], 'AB']])]
+  public function inverse_section_with_else($input, $outcome) {
+    Assert::equals($outcome, $this->evaluate(
+      '{{#people}}{{this}}{{else}}no people{{/people}}',
+      ['people' => $input]
+    ));
+  }
+
+  #[Test, Values([[[], 'no people'], [['A', 'B'], 'AB']])]
+  public function inverse_section_with_short_else($input, $outcome) {
+    Assert::equals($outcome, $this->evaluate(
+      '{{#people}}{{this}}{{^}}no people{{/people}}',
+      ['people' => $input]
+    ));
+  }
+
   #[Test, Values([[[], 'no people'], [['A', 'B'], 'some people']])]
   public function inverse_if_with_else($input, $outcome) {
     Assert::equals($outcome, $this->evaluate(
       '{{^if people}}no people{{else}}some people{{/if}}',
+      ['people' => $input]
+    ));
+  }
+
+  #[Test, Values([[[], 'no people'], [['A', 'B'], 'some people']])]
+  public function inverse_if_with_short_else($input, $outcome) {
+    Assert::equals($outcome, $this->evaluate(
+      '{{^if people}}no people{{^}}some people{{/if}}',
       ['people' => $input]
     ));
   }
